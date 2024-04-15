468f00571d05c476f496a834c63cf3e169c9f156
Mizakson <maxwellizakson@gmail.com>
2024-04-15 10:57:26 -0400

    Add 8 LinkedList class methods
    --> contains(value) method

--- script.js
+++ script.js
@@ -2,7 +2,53 @@
 
 class LinkedList {
     constructor() {
-        this.head = this.head
+        this.list = []
+    }
+
+
+    append(value) {
+        this.list.push(value)
+    }
+
+
+    prepend(value) {
+        this.list.unshift(value)
+    }
+
+
+    size() {
+        return this.list.length
+    }
+
+
+    head() {
+        return this.list[0]
+    }
+
+
+    at(index) {
+        return this.list[index]
+    }
+
+
+    pop() {
+        this.list.pop()
+        return this.list
+    }
+
+
+    contains(value) {
+        for (let i = 0; i < this.list.length; i++) {
+            if (this.list[i].value === value) return true
+            else {
+                return false
+            }
+        }
+    }
+
+
+    find(value) {
+        return null
     }
 
 }
@@ -37,3 +83,23 @@ class Node {
     }
 
 }
+
+
+let test = new LinkedList()
+
+let node1 = new Node()
+node1.setValue(3)
+
+let node2 = new Node()
+node2.setValue(6)
+
+let node3 = new Node()
+node3.setValue(9)
+
+test.append(node1)
+test.append(node2)
+test.append(node3)
+node1.setNextNode(node2.value)
+node2.setNextNode(node3.value)
+
+console.log(test.list)
