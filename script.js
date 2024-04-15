9d9e76b48e1cd1ab865d8a0bf1ab08def58ecb99
Mizakson <maxwellizakson@gmail.com>
2024-04-15 09:59:58 -0400

    Create size method
    LinkedList class

--- script.js
+++ script.js
@@ -16,6 +16,20 @@ class LinkedList {
     }
 
 
+    size() {
+        let counter = 0
+        let node = this.head
+
+        while (node) {
+            counter++
+            node = node.nextNode
+        }
+
+        return counter
+
+    }
+
+
     getHead() {
         return this.head
     }
@@ -45,7 +59,7 @@ class LinkedList {
 
     }
 
-    
+
     toString() {
 
     }
