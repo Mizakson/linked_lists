7e41f78289712993079bca1742f81564c2129516
Mizakson <maxwellizakson@gmail.com>
2024-04-15 10:05:24 -0400

    Create initial LinkedList, Node classes

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // linked list project
 
+class LinkedList {
+    constructor() {
+        this.head = this.head
+    }
+
+}
+
+
+class Node {
+    constructor(value, nextNode) {
+        this.value = null
+        this.nextNode = null
+    }
+}
